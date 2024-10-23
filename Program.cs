33e834487d464365e57fa6f72bf668ac472f0774
soralis0912 <33514613+soralis0912@users.noreply.github.com>
2024-10-24 01:19:08 +0900

    init repo

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using lenovo.mbg.service.common.log;
+
+class LenovoRsaLogDecrypt
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("引数が間違っています");
+            return;
+        }
+        LogAesDecrypt decryptor = new LogAesDecrypt();
+        string encryptFile = args[0];
+
+        string decryptSaveFile = args[1];
+
+        decryptor.Decrypt2File(encryptFile, decryptSaveFile);
+        Console.WriteLine("処理が完了しました。");
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# LenovoRSALogDecrypt
+
+DLLフォルダ直下にlenovo.mbg.service.common.log.dllを配置してください。
+
+
+## 実行方法
+
+LenovoLogDecrypt.exe 暗号化済みファイル 出力ファイル名
+
+
