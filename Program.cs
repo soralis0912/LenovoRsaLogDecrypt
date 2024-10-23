a48e5eecc419429a07704e5dafb2c5c54def18ef
Syuugo <pub@s1204.me>
2024-10-24 06:01:28 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,25 @@
-﻿using lenovo.mbg.service.common.log;
+using System.Diagnostics;
+using lenovo.mbg.service.common.log;
 
-class LenovoRsaLogDecrypt
-{
-    static void Main(string[] args)
-    {
-        if (args.Length != 2)
-        {
-            Console.WriteLine("引数が間違っています");
+class LenovoRsaLogDecrypt {
+    static void Main(string[] args) {
+        if (args.Length != 2) {
+            Console.WriteLine("エラー：引数が間違っています。");
+            Console.WriteLine("使用方法：");
+            Console.WriteLine($"    {Process.GetCurrentProcess().MainModule.FileName} [暗号化済みファイル] [出力ファイル名]");
             return;
         }
-        LogAesDecrypt decryptor = new LogAesDecrypt();
-        string encryptFile = args[0];
 
-        string decryptSaveFile = args[1];
-        try
-        {
-            decryptor.Decrypt2File(encryptFile, decryptSaveFile);
+        LogAesDecrypt decryptor = new LogAesDecrypt();
+        string encryptSrcFile = args[0];
+        string decryptDstFile = args[1];
+
+        try {
+            decryptor.Decrypt2File(encryptSrcFile, decryptDstFile);
             Console.WriteLine("処理が完了しました。");
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
+            Console.WriteLine("エラー：実行に失敗しました。");
+            Console.WriteLine("エラーログ：");
             Console.WriteLine(ex.Message.ToString());
         }
     }
